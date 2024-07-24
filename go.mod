5e0f9c125f1cd3245b089a67a6cfb12728cb0b2e
jasongeee <jasonvgutierrez@gmail.com>
2024-07-23 23:44:31 -0500

    initial go package commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jasongeee/stardew-mods-cli-tool
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	psCommand := `Get-ChildItem -Path C:\ -Recurse -Directory -ErrorAction SilentlyContinue | Where-Object { $_.FullName -like "*\Stardew Valley\Mods" } | Select-Object -ExpandProperty FullName`
+
+	// Create the command to run PowerShell with the desired command
+	cmd := exec.Command("powershell", "-Command", psCommand)
+
+	// Run the command and capture the output
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatal("Error outputting command: ", err)
+		return
+	}
+
+	// Convert the output to a string and trim any whitespace
+	result := strings.TrimSpace(string(output))
+
+	// Print the result
+	fmt.Println("Found directory path:", result)
+}
